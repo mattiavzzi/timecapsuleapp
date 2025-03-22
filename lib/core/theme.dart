33b2b2ec0ada3f421a9d845042c8af9686ff9450
Marco Rossi <marcoreds9724@gmail.com>
2025-03-22 17:30:00 +0100

    Fix divider + Preferences section

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -8,12 +8,13 @@ final appTheme = ThemeData(
     selectedItemColor: Colors.white,
     unselectedItemColor: Colors.grey,
   ),
+  dividerColor: Color.fromRGBO(255, 255, 255, 0.07),
   textTheme: GoogleFonts.interTextTheme().merge(_textTheme),
 );
 
 final _textTheme = TextTheme(
   labelMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w700, color: Colors.white),
-  labelSmall: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: Colors.white),
+  labelSmall: TextStyle(fontSize: 10, fontWeight: FontWeight.w800, color: Colors.white),
   bodyMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.white),
   bodySmall: TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: Colors.white),
 );
